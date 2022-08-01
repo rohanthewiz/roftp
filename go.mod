a0eb0400fdd38f46a4ddabae1664c38479569ebe
Rohan Allison <rohanthewiz@gmail.com>
2022-07-31 20:47:50 -0500

    Switch to go mod, use rerr

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/rohanthewiz/roftp
+
+go 1.17
+
+require (
+	github.com/jlaffaye/ftp v0.0.0-20220630165035-11536801d1ff
+	github.com/rohanthewiz/rerr v0.1.0
+)
+
+require (
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+)
--- roftp.go
+++ roftp.go
@@ -1,19 +1,21 @@
 package roftp
 
 import (
-	"os"
-	"github.com/jlaffaye/ftp"
-	"github.com/rohanthewiz/serr"
-	"path/filepath"
-	"gopkg.in/relistan/rubberneck.v1"
+	"fmt"
 	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/jlaffaye/ftp"
+	"github.com/rohanthewiz/rerr"
 )
 
 type FTPOptions struct {
-	User string
-	Word string
-	Server string
-	Port string
+	User    string
+	Word    string
+	Server  string
+	Port    string
+	Verbose bool
 }
 
 type FileData struct {
@@ -25,23 +27,29 @@ type FileData struct {
 // The first step in using the roftp package is to get a new logged in connection,
 // and cache it locally
 func NewFTPConn(opts FTPOptions) (*ftp.ServerConn, error) {
-	println("Attempting ftp connection")
-	rubberneck.Print(opts)
+	if opts.Verbose {
+		println("Attempting ftp connection...")
+		fmt.Printf("**** FTP Options ->%#v\n", opts)
+	}
 	conn, err := ftp.Connect(opts.Server + ":" + opts.Port)
 	if err != nil {
-		return nil, serr.Wrap(err, "Error connecting to FTP Server")
+		return nil, rerr.Wrap(err, "Error connecting to FTP Server")
 	}
-	println("FTP basic connection established. We still need to login")
+	if opts.Verbose {
+		println("FTP basic connection established. We still need to login")
+	}
 	err = Login(conn, opts)
 	if err != nil {
-		return nil, serr.Wrap(err, "Error logging in to ftp server")
+		return nil, rerr.Wrap(err, "Error logging in to ftp server")
 	}
 	return conn, nil
 }
 
 // Login on the supplied basic connection
 func Login(conn *ftp.ServerConn, opts FTPOptions) error {
-	println("Attempting to Login...")
+	if opts.Verbose {
+		println("Attempting to Login...")
+	}
 	return conn.Login(opts.User, opts.Word)
 }
 
@@ -51,13 +59,13 @@ func Login(conn *ftp.ServerConn, opts FTPOptions) error {
 func ListFiles(conn *ftp.ServerConn, serverPath string) (filesData []FileData, err error) {
 	currPath, err := ChDir(conn, serverPath)
 	if err != nil {
-		return filesData, serr.Wrap(err, "Unable to obtain current directory")
+		return filesData, rerr.Wrap(err, "Unable to change current dir")
 	}
 	println("Current path:", currPath)
 
 	entries, err := conn.List("")
-	if  err != nil {
-		return nil, serr.Wrap(err, "Error listing files", "currentDir", currPath)
+	if err != nil {
+		return nil, rerr.Wrap(err, "Error listing files", "currentDir", currPath)
 	}
 	println(len(entries), "file(s) found at", currPath)
 	for _, entry := range entries {
@@ -68,7 +76,7 @@ func ListFiles(conn *ftp.ServerConn, serverPath string) (filesData []FileData, e
 		case ftp.EntryTypeFolder:
 			fileType = "directory"
 		}
-		filesData = append(filesData, FileData{ Name: entry.Name, Size: entry.Size, Type: fileType })
+		filesData = append(filesData, FileData{Name: entry.Name, Size: entry.Size, Type: fileType})
 	}
 	return
 }
@@ -76,11 +84,11 @@ func ListFiles(conn *ftp.ServerConn, serverPath string) (filesData []FileData, e
 // Change directory and return the new path or err
 func ChDir(conn *ftp.ServerConn, serverPath string) (currPath string, err error) {
 	if err = conn.ChangeDir(serverPath); err != nil {
-		return "", serr.Wrap(err, "Error changing directory on ftp server", "requestedPath", serverPath)
+		return currPath, rerr.Wrap(err, "Error changing directory on ftp server", "requestedPath", serverPath)
 	}
 	currPath, err = conn.CurrentDir()
 	if err != nil {
-		return "", serr.Wrap(err, "Unable to obtain server's current directory after changing directory")
+		return currPath, rerr.Wrap(err, "Unable to obtain server's current directory after changing directory")
 	}
 	return
 }
@@ -91,44 +99,64 @@ func ChDir(conn *ftp.ServerConn, serverPath string) (currPath string, err error)
 func UploadFile(conn *ftp.ServerConn, srcFullPath, serverPath string, destFilename ...string) error {
 	file, err := os.Open(srcFullPath)
 	if err != nil {
-		return serr.Wrap(err, "Unable to open file for upload")
+		return rerr.Wrap(err, "Unable to open file for upload")
 	}
-	defer file.Close()
+	defer func() {
+		_ = file.Close()
+	}()
 
 	if len(destFilename) > 0 {
 		serverPath = filepath.Join(serverPath, destFilename[0])
 	}
 	// Upload
-	println("Uploading sermon:", srcFullPath)
+	println("Uploading:", srcFullPath) // TODO Show srcFullPath ==> serverPath + destFilename
 	err = conn.Stor(serverPath, file)
 	if err != nil {
-		return serr.Wrap(err, "Error uploading file", "actual_server_path", serverPath)
+		return rerr.Wrap(err, "Error uploading file", "actual_server_path", serverPath)
 	}
+	println("Upload of", srcFullPath, "completed")
 	return err
 }
 
 // Download and write file from server
 func DownloadFiles(conn *ftp.ServerConn, serverPath string, limit ...int) (success int, fail int, err error) {
 	items, err := ListFiles(conn, serverPath)
-	if err != nil { return 0, 0, serr.Wrap(err, "Could not obtain dir entries by name") }
+	if err != nil {
+		return 0, 0, rerr.Wrap(err, "Could not obtain dir entries by name")
+	}
 
 	lmt := 0
-	if len(limit) > 0 { lmt = limit[0] }
+	if len(limit) > 0 {
+		lmt = limit[0]
+	}
 	println("The download limit is", lmt)
 	for _, item := range items {
-		if lmt != 0 && success + 1 > lmt { break }
-		if item.Type != "file" || item.Name == "." || item.Name == ".." { continue }
+		if lmt != 0 && success+1 > lmt {
+			break
+		}
+		if item.Type != "file" || item.Name == "." || item.Name == ".." {
+			continue
+		}
 		println("Downloading", item.Name)
 		err = DownloadFile(conn, serverPath, item.Name)
-		if err != nil {	fail++; println(err.Error()); continue }
+		if err != nil {
+			fail++
+			println(err.Error())
+			continue
+		}
 		success++
 	}
 	return
 }
 func DownloadFile(conn *ftp.ServerConn, serverPath, destName string) error {
 	data, err := DownloadFileBuffer(conn, filepath.Join(serverPath, destName))
-	if err != nil {	return err	}
-	ioutil.WriteFile(destName, data, 0664)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(destName, data, 0664)
+	if err != nil {
+		return rerr.Wrap(err)
+	}
 	return nil
 }
 
@@ -137,14 +165,14 @@ func DownloadFileBuffer(conn *ftp.ServerConn, serverPath string) (fileData []byt
 	println("Downloading", serverPath, "to buffer")
 	resp, err := conn.Retr(serverPath)
 	if err != nil {
-		return nil, serr.Wrap(err, "Error downloading file from server", "remote_file", serverPath)
+		return nil, rerr.Wrap(err, "Error downloading file from server", "remote_file", serverPath)
 	}
-	//resp.SetDeadline(time.Now().Add(time.Minute * 15))
+	// resp.SetDeadline(time.Now().Add(time.Minute * 15))
 	fileData, err = ioutil.ReadAll(resp)
 	if err != nil {
-		return nil, serr.Wrap(err, "Error reading file from server", "remote_file", serverPath)
+		return nil, rerr.Wrap(err, "Error reading file from server", "remote_file", serverPath)
 	}
 	println(len(fileData), "bytes read")
-	resp.Close()
+	_ = resp.Close()
 	return
 }
